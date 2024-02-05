9513f075bc1e28bfc2aaa7488cd06d771b7e8206
Gene Goltsman <egoltsman@gmail.com>
2024-02-05 06:30:11 +0000

    minor cleanup

--- workflow.py
+++ workflow.py
@@ -25,10 +25,7 @@ if __name__ == "__main__":
 
     vcf_in = args.vcf
     if not os.path.exists(vcf_in):
-        raise SystemExit('Input vcf file does not exist')
-    
-
-
+        raise SystemExit("Input vcf file does not exist")
 
     if args.out_pref:
         vcf_annot = args.out_pref + ".ann.vcf"
